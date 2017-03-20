fb86c7a64977b3ede3e5746497f573fcc37e4a7f
Vytautas <vytautas@eface.lt>
2017-03-21 00:27:51 +0200

    unused code removed

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -29,7 +29,7 @@ class BlogController extends Controller
      *
      * @Route("/blog/{slug}", name="blog_show")
      */
-    public function showAction(Request $request, $slug)
+    public function showAction(Request $request)
     {
         return $this->returnResponse($request);
     }
